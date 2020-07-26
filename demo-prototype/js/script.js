59260debcdbba53ef906f548eca87c9e41406c43
Anoushka Sengupta <51234849+ankasengupta@users.noreply.github.com>
2020-07-26 23:26:44 +0530

    added prototype website

---
+++ demo-prototype/js/script.js
@@ -0,0 +1,47 @@
+function setbackground()
+{
+window.setTimeout( "setbackground()", 1000); // 1000 milliseconds (1 second) delay
+
+var index = Math.round(Math.random() * 9);
+var colorValue = "FFFFFF"; // default color - white (index = 0)
+
+// hard coding values for prototype
+switch(index) {
+    case 1:
+        colorValue = "a6beff"; // light blue
+        break;
+    case 2 :
+        colorValue = "bba6ff"; // lilac
+        break;
+    case 3:
+        colorValue = "beffa6"; // light green
+        break;
+    case 4 :
+        colorValue = "ffa6be"; // light pink
+        break;
+    case 5:
+        colorValue = "a6ebff"; // lighter blue
+        break;
+    case 6:
+        colorValue = "ffe7a6"; // light yellow
+        break;
+    case 7:
+        colorValue = "ffbba6"; // peach
+        break;
+    case 8 :
+        colorValue = "ffa6ea"; // light-ish pink
+        break;
+    case 9:
+        colorValue = "eaffa6"; // yellow but different
+        break;
+    case 10 :
+        colorValue = "a6ffe8"; // turquoise
+        break;
+}
+
+var red = Math.round(Math.random() * 254);
+var blue = Math.round(Math.random() * 254);
+var green = Math.round(Math.random() * 254);
+
+document.getElementsByTagName("body")[0].style.backgroundColor = `#${colorValue}`;
+}
